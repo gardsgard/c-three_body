a349960f0a353393f0bd707395d24139c6a39414
Mattias Linder <mattias.linder87@gmail.com>
2026-03-03 20:26:37 +0100

    migrate to nob

---
+++ nob.c
@@ -0,0 +1,24 @@
+#define NOB_IMPLEMENTATION
+
+#include "nob.h"
+
+#define BUILD_FOLDER "build/"
+#define SRC_FOLDER   "src/"
+
+int main(int argc, char **argv) {
+    NOB_GO_REBUILD_URSELF(argc, argv);
+
+    if (!nob_mkdir_if_not_exists(BUILD_FOLDER)) return 1;
+
+	Nob_Cmd cmd = {0};
+	nob_cmd_append(&cmd, "clang","-Wall", "-Wextra", "-o", BUILD_FOLDER"three-body", SRC_FOLDER"three-body.c");
+	nob_cmd_append(&cmd, "-I./deps/macos_arm", "-Ldeps/macos_arm/", "deps/macos_arm/libraylib.a");
+	nob_cmd_append(&cmd, "-framework", "CoreVideo", "-framework","IOKit", "-framework", "Cocoa", "-framework", "GLUT", "-framework", "OpenGL");
+
+	if (!nob_cmd_run(&cmd)) return 1;
+
+	nob_cmd_append(&cmd, "./build/three-body");
+	if (!nob_cmd_run(&cmd)) return 1;
+
+	return 0;
+}
