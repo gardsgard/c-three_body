3225cd6787f180225a74bfc2fa2965d5181fa5f3
Mattias Linder <mattias.linder87@gmail.com>
2026-03-03 21:06:48 +0100

    updated nob.c

--- nob.c
+++ nob.c
@@ -13,7 +13,11 @@ int main(int argc, char **argv) {
 	Nob_Cmd cmd = {0};
 	nob_cmd_append(&cmd, "clang","-Wall", "-Wextra", "-o", BUILD_FOLDER"three-body", SRC_FOLDER"three-body.c");
 	nob_cmd_append(&cmd, "-I./deps/macos_arm", "-Ldeps/macos_arm/", "deps/macos_arm/libraylib.a");
-	nob_cmd_append(&cmd, "-framework", "CoreVideo", "-framework","IOKit", "-framework", "Cocoa", "-framework", "GLUT", "-framework", "OpenGL");
+	nob_cmd_append(&cmd, "-framework", "CoreVideo");
+	nob_cmd_append(&cmd, "-framework", "IOKit");
+	nob_cmd_append(&cmd, "-framework", "Cocoa");
+	nob_cmd_append(&cmd, "-framework", "GLUT");
+	nob_cmd_append(&cmd,  "-framework", "OpenGL");
 
 	if (!nob_cmd_run(&cmd)) return 1;
 
